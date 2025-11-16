664b7d1e41892fb7f74b4bd23e9e3afdc2af6951
Ahmed Abd El-Mageed <44370339+ahmedabdelmageed1710@users.noreply.github.com>
2025-11-16 20:07:20 +0200

    update return message

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 const express = require('express');
-// const mongoose = require('mongoose');
-const {Pool , Client} = require('pg');
+const mongoose = require('mongoose');
+// const {Pool , Client} = require('pg');
 const redis = require('redis');
 
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-const REDIS_HOST = 'redis';
-const REDIS_PORT = 6379;
+// const REDIS_HOST = 'redis';
+// const REDIS_PORT = 6379;
 
 // Create Redis client
 const redisClient = redis.createClient({
-  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
 });  
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 redisClient.on('connect', () => console.log('Redis client connected'));
 redisClient.connect();
-
 
 // Connect to MongoDB
 // const DB_USER = 'root';
@@ -26,29 +25,26 @@ redisClient.connect();
 // const DB_PORT = 27017;
 // const DB_HOST = 'mongo';
 
-// const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-// mongoose.connect(URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-// .then(() => console.log('MongoDB connected'))
-// .catch(err => console.error('MongoDB connection error:', err));
+const URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
+mongoose.connect(URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 
 // Connect to PostgreSQL
-const DB_USER = 'root';
-const DB_PASSWORD = 'example';
-const DB_PORT = 5432;
-const DB_HOST = 'postgres';
+// const DB_USER = 'root';
+// const DB_PASSWORD = 'example';
+// const DB_PORT = 5432;
+// const DB_HOST = 'postgres';
 
-const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-const client = new Client({
-  connectionString: URI,
-});
+// const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+// const client = new Client({
+//   connectionString: URI,
+// });
 
-client.connect()
-.then(() => console.log('PostgreSQL connected'))
-.catch(err => console.error('PostgreSQL connection error:', err));
+// client.connect()
+// .then(() => console.log('PostgreSQL connected'))
+// .catch(err => console.error('PostgreSQL connection error:', err));
 
 
 // Sample route
@@ -59,7 +55,7 @@ app.get('/', (req, res) => {
 
 app.get('/data', async (req, res) => {
   const products = await redisClient.get('products');
-  res.send(`<h1>Hello World! dev</h1> <h2>${products}</h2>`);
+  res.send(`<h1>Hello World! from docker hub</h1> <h2>${products}</h2>`);
 });
 
 // Start the server
